ab6f6e939130830ecbd432cf7f195c9f3740f2e0
John Ginnane <John.Ginnane1@Gmail.com>
2019-08-21 08:40:23 +0100

    added sine wave generator

--- tts/Program.cs
+++ tts/Program.cs
@@ -9,9 +9,10 @@ namespace tts
     {
         static void Main(string[] args)
         {
-            wave test = new wave();
-            test.generateSquare(440f, 0.5f, 15000);
-            test.saveToFile("test.wav");
+            wave test = new wave(2, 11025);
+            //test.generateSquare(440f, 0.5f, 15000);
+            test.generateSine(261.63, length: 15 * 1000);
+            test.saveToWAV("test.wav");
             Console.ReadKey();
         }
     }
@@ -30,7 +31,7 @@ namespace tts
         private ushort blockAlign => (ushort)(numChannels * bitsPerSample / 8);
         ushort bitsPerSample = 16;
         string subChunk2ID = "data";
-        private uint subChunk2Size => (uint)(data.Length * numChannels * bitsPerSample / 8);
+        private uint subChunk2Size => (uint)(data.Length * bitsPerSample / 8);
         public int[] data;
 
         public wave(ushort numChannels = 1, uint sampleRate = 11025)
@@ -39,7 +40,7 @@ namespace tts
             this.sampleRate = sampleRate;
         }
 
-        public void saveToFile(string path)
+        public void saveToWAV(string path)
         {
             // http://soundfile.sapp.org/doc/WaveFormat/
             // https://stackoverflow.com/questions/14659684/creating-a-wav-file-in-c-sharp
@@ -78,21 +79,44 @@ namespace tts
             bw.Write(bitsPerSample);
             bw.Write(System.Text.Encoding.ASCII.GetBytes(subChunk2ID));
             bw.Write(subChunk2Size);
-            foreach (int v in data)
+            foreach (short v in data)
             {
-                if (bitsPerSample == 8)
-                {
-                    bw.Write((byte)v);
-                } else
-                {
-                    bw.Write(v);
-                }
+                bw.Write(v);
             }
 
             fs.Close();
             fs.Dispose();
             bw.Close();
             bw.Dispose();
+        }
+
+        public void generateSine(double frequency = 440.0f, float volume = 0.5f, uint length = 1000)
+        {
+            // http://pages.mtu.edu/~suits/notefreqs.html
+            // https://blogs.msdn.microsoft.com/dawate/2009/06/24/intro-to-audio-programming-part-3-synthesizing-simple-wave-audio-using-c/
+            double t = (Math.PI * 2 * frequency) / (sampleRate * numChannels);
+            uint samples = (uint)(sampleRate * (double)(length / 1000.0f));
+            data = new int[samples * numChannels];
+            int ampOffset = 0;
+            int amplitude = 0;
+
+            if (bitsPerSample == 8)
+            {
+                ampOffset = byte.MaxValue / 2;
+                amplitude = (int)(((float)byte.MaxValue / 2.0f) * volume);
+            } else if (bitsPerSample == 16)
+            {
+                ampOffset = 0;
+                amplitude = (int)((float)short.MaxValue * volume);
+            }
+
+            for (uint i = 0; i < samples - 1; i++)
+            {
+                for (short channel = 0; channel < numChannels; channel++)
+                {
+                    data[i + channel] = Convert.ToInt16(ampOffset + amplitude * Math.Sin(t * i));
+                }
+            }
         }
 
         public void generateSquare(float frequency = 440.0f, float volume = 0.5f, uint length = 1000)
